012186a80a4c8445501450e029c20e53d40d4af6
Felipe <felipe.carlos1504@gmail.com>
2019-03-17 14:58:35 -0300

    [add] aviso app.component, e futura navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-core-root></app-core-root>`,
-  styles: [``]
+  template: `<app-core-root></app-core-root>`
 })
-export class AppComponent implements OnInit {
-  constructor(
-  ) {
 
-  }
-
-  ngOnInit() {
-  }
-}
+/**
+ * IMPORTANTE! Não é necessário configurar nada no app.component, para deixa-lo limpo,
+ * qualquer configuração que você irá fazer aqui pode ser feita no core.component!
+ * A arquitetura do app foi preparada para isso!
+ */
+export class AppComponent {}
--- src/app/core/core.component.ts
+++ src/app/core/core.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-core-root',
-    template: `<router-outlet></router-outlet>`,
+    template: `
+        <nav>NavBar</nav>
+        <router-outlet></router-outlet>
+    `,
     styles: [``]
 })
 export class CoreComponent implements OnInit {
